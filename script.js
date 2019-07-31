23be344a0fe1ea61107efc10e8e939439d345a02
Erick García <e.gch.2895@gmail.com>
2019-07-31 17:11:05 -0500

    Init version

---
+++ script.js
@@ -0,0 +1,10 @@
+﻿window.onload = () => {
+    document.querySelector('#hbd-btn')
+        .addEventListener('click', (e) => {
+            const el = e.target;
+            el.classList.add('clicked');
+            setTimeout(() => {
+                el.classList.remove('clicked');
+            }, 1500);
+        });
+}
